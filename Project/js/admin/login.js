3c729c0351e29133afaceb55b49185fa49fc20a5
Nguyen Phu Huy <huybean160999@gmail.com>
2019-06-12 21:20:23 +0700

    hoan thanh

--- Project/js/admin/login.js
+++ Project/js/admin/login.js
@@ -2,6 +2,12 @@ $('#login').click(function() {
       		if ($('#name').val()=='admin' && $('#pass').val()=='123456') {
       			window.location.href='index.html';
       		}else{
-				$( ".alert-danger" ).show();
+				if ($('#name').val()=='giaovien' && $('#pass').val()=='123456') {
+      			window.location.href='index2.html';
+	      		}else{
+					$( ".alert-danger" ).show();
+				}
 			}
+
+			
 	});
